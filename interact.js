49428bf1a76d5c602cd6f682fed8570df271ae4a
fabian <fabian@MacBook-Air-de-fabian.local>
2023-08-11 14:11:10 -0300

    updating for a simulaPurchase

--- interact.js
+++ interact.js
@@ -1,43 +1,91 @@
-const ethers = require('ethers');
+const { ethers } = require('ethers');
 const { JsonRpcProvider } = require('ethers/providers');
-const { utils } = require('ethers');
-console.log(utils)
 const fs = require('fs');
 
 // Load environment variables
 require('dotenv').config();
 
-
-
 console.log("Starting script...");
 
-
-// Connect to Matic's Mumbai network using ethers
 const provider = new JsonRpcProvider(process.env.MATIC_RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-console.log(process.env.PRIVATE_KEY);
-console.log(wallet);
-console.log(utils); 
+
 console.log("Connected to Matic network...");
 console.log("Loaded account:", wallet.address);
 
 const contractData = JSON.parse(fs.readFileSync('out/market_transaction.sol/ShoppingTransaction.json', 'utf8'));
-const contract = new ethers.Contract('0x00F382A1E79b4159071faF7a608bD3F987cb9501', contractData.abi, wallet);
-console.log("Contract instantiated at:", contract.address);
+const contract = new ethers.Contract('0x99B5d4F29Cc6E8165245764cEeCdD37dA94F1f07', contractData.abi, wallet);
+contract.address = '0x99B5d4F29Cc6E8165245764cEeCdD37dA94F1f07';
+console.log("Contract instantiated at:", '0x99B5d4F29Cc6E8165245764cEeCdD37dA94F1f07');
 
-async function checkBalance() {
-    let balanceWei = await wallet.getBalance();
-    let balance = ethers.utils.formatEther(balanceWei);
-    console.log(`Balance for account ${wallet.address}: ${balance} ETH (or Matic if you're on that network)`);
+const erc20Abi = [ 
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "name": "amount",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+async function checkAddresses() {
+    const usdtAddress = await contract.usdtToken();
+    const usdcAddress = await contract.usdcToken();
+
+    console.log("USDT Address in Contract:", usdtAddress);
+    console.log("USDC Address in Contract:", usdcAddress);
 }
 
-checkBalance();
+
+async function checkBalance() {
+    let balanceWei = await provider.getBalance(wallet.address);
+    let balance = ethers.formatEther(balanceWei);
+    console.log(`Balance for account ${wallet.address}: ${balance} Matic`);
+}
+
+
+async function approveTokens(tokenAddress, amount) {
+    if (!tokenAddress) {
+        console.error("Token address is not defined!");
+        return;
+    }
+    console.log(`Approving tokens. Token address: ${tokenAddress}, Amount: ${amount}`);
+
+    console.log("Approving contract address:", contract.address); 
+
+    const token = new ethers.Contract(tokenAddress, erc20Abi, wallet);
+    const tx = await token.approve(contract.address, amount);
+    await tx.wait();
+    console.log(`Tokens approved for contract: ${amount}`);
+}
+
 
 async function simulatePurchase() {
     try {
         console.log("Making purchase...");
-        const totalToPay = utils.parseEther('25'); // Simulate a 25 USDT purchase
+        const totalToPay = ethers.parseUnits('2', 18);// Simulate a 2 USDT purchase
         const orderUUID = "123456"; // Just an example, you should generate a unique UUID in practice
+        console.log("USDC_ADDRESS:", process.env.USDT_ADDRESS);
+
+        console.log("Token Contract Address:", process.env.USDT_ADDRESS);
+        console.log("Shopping Contract Address:", contract.address);
+
+        await approveTokens(process.env.USDT_ADDRESS, totalToPay); // Ensure to set the USDT_ADDRESS in your .env file
 
         const transactionResponse = await contract.purchase(orderUUID, totalToPay, 0, { gasLimit: 2000000 }); // 0 is for USDT, 1 for USDC
         console.log("Transaction sent. Awaiting confirmation...");
@@ -48,5 +96,7 @@ async function simulatePurchase() {
         console.error("An error occurred during purchase simulation:", error);
     }
 }
-
+checkAddresses();
+checkBalance();
 simulatePurchase();
+
